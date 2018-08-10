ba075bc56ff4d4bfa23372ea836aae54bbdd2748
Chris Bednarski <chris@cbednarski.com>
2018-08-10 16:42:05 -0700

    Initial import of jsonf program and tests

---
+++ main.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	formatCompact = "c"
+	formatString = "s"
+	formatTab = "t"
+)
+
+const helpText = `jsonf - A simple JSON formatter
+
+When invoked on a single file, the file will be reformatted and the output sent
+to stdout. You may rewrite the original file using the -w flag.
+
+When invoked on a directory, the program will only operate on files ending in
+.json.
+
+When processing multiple files the program will output the filename of each file
+before it is processed, and will attempt to continue when it encouters errors.
+
+Usage:
+
+  jsonf [options] filename
+  jsonf [options] directory
+
+Example:
+
+  jsonf -i sss myfile.json    Indent using 3 spaces and write to stdout
+  jsonf -w -i t myfile.json   Indent using tabs and rewrite the original files
+  jsonf -w .                  Rewrite all .json files in the current directory
+
+Options:
+
+  -w  Overwrite files in place
+  -r  Recurse into subdirectories
+  -i  Set the indentation string (defaults to 2 spaces). You can use 's' and 't'
+      to replace literal space and tab characters
+  -c  Compact (minify) rather than indent. -c wins over -i if both are specified
+  -h  Show help
+
+Report issues to https://github.com/cbednarski/jsonf
+`
+
+var (
+	exitCode = 0
+	stdout = os.Stdout
+	stderr = os.Stderr
+)
+
+func printError(err error) {
+	stderr.WriteString(fmt.Sprintf("%s\n", err))
+	exitCode = 1
+}
+
+func format(filename string, indent string) (*bytes.Buffer, error) {
+	output := &bytes.Buffer{}
+
+	input, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	input = bytes.TrimSpace(input)
+
+	if indent == formatCompact {
+		if err := json.Compact(output, input); err != nil {
+			return nil, err
+		}
+	} else {
+		if err := json.Indent(output, input, "", indent); err != nil {
+			return nil, err
+		}
+		output.WriteString("\n")
+	}
+
+	return output, nil
+}
+
+func formatFile(filename string, indent string, replace bool) error {
+	data, err := format(filename, indentString(indent))
+	if err != nil {
+		return err
+	}
+
+	if replace {
+		info, err := os.Stat(filename)
+		if err != nil {
+			return err
+		}
+
+		if err := ioutil.WriteFile(filename, data.Bytes(), info.Mode()); err != nil {
+			return err
+		}
+	} else {
+		data.WriteTo(stdout)
+	}
+
+	return nil
+}
+
+func indentString(input string) string {
+	// Special case for compaction
+	if strings.Contains(input, formatCompact) {
+		return formatCompact
+	}
+
+	input = strings.Replace(input, formatString, " ", -1)
+	input = strings.Replace(input, formatTab, "\t", -1)
+
+	return input
+}
+
+func listJSONFiles(path string, recurse bool) ([]string, error) {
+	files := []string{}
+	if isDir(path) {
+		filepath.Walk(path, func(innerPath string, info os.FileInfo, err error) error{
+			// We always want to talk the current directory, but not any
+			// subdirectories, unless recurse is true.
+			if !recurse && info.IsDir() && innerPath != path {
+				return filepath.SkipDir
+			}
+
+			if !info.IsDir() && strings.HasSuffix(info.Name(), ".json") {
+				files = append(files, innerPath)
+			}
+
+			return nil
+		})
+	} else {
+		files = []string{path}
+	}
+
+	return files, nil
+}
+
+func isDir(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return info.IsDir()
+}
+
+func formatPath(path, indent string, replace, recurse bool) error {
+	if isDir(path) {
+		files, err := listJSONFiles(flag.Args()[0], recurse)
+		if err != nil {
+			return err
+		}
+		errors := 0
+		for _, f := range files {
+			fmt.Println(f)
+			if err := formatFile(f, indent, replace); err != nil {
+				printError(err)
+			}
+		}
+		if errors > 0 {
+			return fmt.Errorf("Encountered %d errors in %s", errors, path)
+		}
+	} else {
+		if err := formatFile(path, indent, replace); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func wrappedMain() error {
+	replace := flag.Bool("w", false, "overwrite files in place")
+	recurse := flag.Bool("r", false, "recurse into subdirectories")
+	compact := flag.Bool("c", false, "compact instead of intent")
+	indent := flag.String("i", "  ", `indent string, e.g. "  "`)
+	help := flag.Bool("h", false, "show help")
+
+	flag.Parse()
+
+	if *help {
+		fmt.Println(helpText)
+		os.Exit(0)
+	}
+
+	if *compact {
+		*indent = formatCompact
+	}
+
+	if len(flag.Args()) < 1 {
+		return errors.New(helpText)
+	}
+
+	for _, path := range flag.Args() {
+		if err := formatPath(path, *indent, *replace, *recurse); err != nil {
+			printError(err)
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	if err := wrappedMain(); err != nil {
+		stderr.WriteString(fmt.Sprintf("%s\n", err))
+		os.Exit(1)
+	}
+	os.Exit(exitCode)
+}
---
+++ main_test.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+func TestIsDir(t *testing.T) {
+	if !isDir("test-fixtures") {
+		t.Errorf("test-fixtures should be a directory")
+	}
+
+	if isDir("main.go") {
+		t.Errorf("main.go should not be a directory")
+	}
+}
+
+var expectedFormatSpaces = `{
+  "icecream": [
+    "chocolate",
+    "strawberry",
+    "vanilla"
+  ]
+}
+`
+
+var expectedFormatTabs = `{
+	"icecream": [
+		"chocolate",
+		"strawberry",
+		"vanilla"
+	]
+}
+`
+
+var expectedFormatCompact = `{"icecream":["chocolate","strawberry","vanilla"]}`
+
+type FormatTest struct {
+	InputFile string
+	Indent string
+	Expected string
+}
+
+func TestFormat(t *testing.T) {
+	inputs := map[string]FormatTest{
+		"spaces": {
+			InputFile: filepath.Join("test-fixtures", "icecream.json"),
+			Indent: "  ",
+			Expected: expectedFormatSpaces,
+		},
+		"tabs": {
+			InputFile: filepath.Join("test-fixtures", "icecream.json"),
+			Indent: "\t",
+			Expected: expectedFormatTabs,
+		},
+		"compact": {
+			InputFile: filepath.Join("test-fixtures", "icecream.json"),
+			Indent: formatCompact,
+			Expected: expectedFormatCompact,
+		},
+	}
+
+	for testCase, input := range inputs {
+		actual, err := format(input.InputFile, input.Indent)
+		if err != nil {
+			t.Errorf("Test case %q error: %s", testCase, err)
+			continue
+		}
+
+		actualString := actual.String()
+		if actualString != input.Expected {
+			t.Errorf("Test case %q doesn't match\n--Expected--\n%s\n--Actual--\n%s", testCase, input.Expected, actualString)
+		}
+	}
+}
+
+func TestIndentString(t *testing.T) {
+	actual := indentString("sts ")
+	expected := " \t  "
+	if actual != expected {
+		t.Errorf("Expected %q, found %q", expected, actual)
+	}
+
+	actual2 := indentString("cststs")
+	expected2 := "c"
+	if actual2 != expected2 {
+		t.Errorf("Expected %q, found %q", expected, actual)
+	}
+}
+
+func TestListJSONFiles(t *testing.T) {
+	expectedFiles1 := []string{filepath.Join("test-fixtures", "icecream.json")}
+
+	actualFiles1, err := listJSONFiles("test-fixtures", false)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(expectedFiles1, actualFiles1) {
+		t.Errorf("No match. Expected %v, found %v", expectedFiles1, actualFiles1)
+	}
+}
+
+func TestListJSONFilesRecursive(t *testing.T) {
+	expectedFiles2 := []string{
+		filepath.Join("test-fixtures", "dogs", "corgi.json"),
+		filepath.Join("test-fixtures", "dogs", "dachshund.json"),
+		filepath.Join("test-fixtures", "icecream.json"),
+	}
+
+	actualFiles2, err := listJSONFiles("test-fixtures", true)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(expectedFiles2, actualFiles2) {
+		t.Errorf("No match. Expected %v, found %v", expectedFiles2, actualFiles2)
+	}
+}
+
+func TestFormatFile(t *testing.T) {
+	data, err := ioutil.ReadFile(filepath.Join("test-fixtures", "icecream.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	file, err := ioutil.TempFile("", "jsonf-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	filename := file.Name()
+
+	file.Write(data)
+	file.Close()
+
+	defer os.Remove(filename)
+
+	if err := formatFile(filename, "ss", true); err != nil {
+		t.Fatal(err)
+	}
+
+	actual, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actualString := string(actual)
+
+	if actualString != expectedFormatSpaces {
+		t.Errorf("Output does not match:\n--Expected--\n%s\n--Actual--\n%s", expectedFormatSpaces, actualString)
+	}
+}
